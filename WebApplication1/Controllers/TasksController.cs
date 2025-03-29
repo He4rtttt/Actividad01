34f7b6d586f5d9c45fa033c0a0f943244da6f5cc
LuisHuertaGonzales <wosowaf@gmail.com>
2025-03-28 20:52:54 -0500

    LuisHuerta_ParteControlador

---
+++ WebApplication1/Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class TasksController : Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Title = "Hacer la compra", Status = "Pendiente" },
+            new TaskItem { Id = 2, Title = "Estudiar .NET", Status = "Completada" }
+        };
+
+        // GET /tasks
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetTasks()
+        {
+            return Ok(tasks);
+        }
+}
