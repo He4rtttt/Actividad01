f5de0be693c41937875ad92bb90ecafc3b68923d
He4rtttt <kevin.caya@tecsup.edu.pe>
2025-03-29 21:50:57 -0500

    Kevin Caya Modificaciones

---
+++ WebApplication1/Models/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Status { get; set; } = "Pendiente"; // "Pendiente" o "Completada"
+}
